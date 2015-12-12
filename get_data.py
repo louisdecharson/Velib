e24fd8ab7a53cef7a6e381fe5ceb04b5e1ff83c5
Louis de Charson <ltdc2121@hotmail.fr>
2015-12-12 23:08:44 +0100

    .csv is now created each time the program is launched and received a name with date and time

--- get_data.py
+++ get_data.py
@@ -2,17 +2,38 @@
 # -*- coding: utf-8 -*-
 # ------------ DONNEES.PY --------------
 
+# Python Packages
 import urllib.request
 import pandas as pd
 import time
 import re
 import json
 from pandas.io.json import json_normalize
+from time import gmtime, strftime
+
+
+# R Packages
 import rpy2.robjects as robjects
+from rpy2.robjects.packages import importr
+r=robjects.r
+from rpy2.robjects.packages import STAP
+
 
 url = "http://opendata.paris.fr/api/records/1.0/search/?dataset=stations-velib-disponibilites-en-temps-reel&rows=1240&facet=banking&facet=bonus&facet=status&facet=contract_name"
 
-for i in range(0,2):
+myDataDir="/Users/louisdecharson/Programmation/Python/Velib/data/"
+
+def create_map_with_R(path_to_csv):
+    with open('plot.R', 'r') as f:
+        string = f.read()
+    plot_R = STAP(string, "plot_R")
+    plot_R.plotMyMap(path_to_csv)
+
+    # r.assign('path_to_csv', path_to_csv)
+    # r['source']("plot.R"
+
+
+for i in range(0,1):
     response = urllib.request.urlopen(url)
     data=str(response.read()).strip()
 
@@ -30,11 +51,19 @@ for i in range(0,2):
     df['longitude']=longitude
     df['latitude'] = latitude
 
+    df.rename(columns={'fields.address': 'address', 'fields.available_bike_stands': 'available_bike_stands','fields.available_bikes' : 'available_bikes', 'fields.bonus' : 'bonus', 'fields.last_update' : 'last_update', 'fields.number' : 'station_number', 'fields.status' : 'status', 'fields.bike_stands' :'bike_stands' }, inplace=True)
+    df.drop('fields.banking', axis=1, inplace=True)
+    df.drop('fields.contract_name', axis=1, inplace=True)
+    df.drop('fields.name', axis=1, inplace=True)
+
+
     #Sauvegarde dans un .csv
-    df.to_csv("/Users/louisdecharson/Programmation/Python/Velib/data/velib.csv")
+    myClock=strftime("%Y_%m_%d__%H_%M_%S", gmtime())
+    path_to_csv=myDataDir+"veliv_"+myClock+".csv"
+    df.to_csv(path_to_csv)
+    
     #Lancement du script R.
-    r=robjects.r
-    r['source']("plot.R")
+    create_map_with_R(path_to_csv)
     time.sleep(60)
 
 
